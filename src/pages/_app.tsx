2a211b59b84a7708b4d85a55dfd482028ad7d5ac
Ebenezer Ekunke (NexDev) <nezerekunke@gmail.com>
2026-01-09 14:53:16 +0100

    Add Head component for title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,3 +4,19 @@ import type { AppProps } from "next/app";
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
 }
+
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Nezer Peppermint Assessment</title>
+        <link rel="shortcut icon" href="/assets/logo-purple.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
