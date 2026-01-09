9aba0be0e5505b44efb99969ca6992d11958b5d7
Ebenezer Ekunke (NexDev) <nezerekunke@gmail.com>
2026-01-09 14:54:05 +0100

    Refactor App component in _app.tsx
    Removed redundant imports and cleaned up the component structure.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,5 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
-
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
 import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
